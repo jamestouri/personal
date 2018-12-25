f45aee41a4296fb383d58163f1d10abb82f6020a
jamestouri <j_touri@yahoo.com>
2018-12-25 10:33:36 -0800

    add images

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,14 +4,24 @@ import '../stylesheets/project.css';
 const ProjectPage = () => {
   return (
     <div className="projects">
-      <div>
+      <div className="porcelain">
         <h1>Porcelain</h1>
+        <p>A single page web application that crowdsources restrooms and reviews.
+          Great for Ride-Sharing drivers that need to find clean restrooms.
+          Built with React, Redux, Javascript, Node.js and MongoDB.
+          Integrates with Google Maps Api</p>
       </div>
-      <div>
+      <div className="togather">
         <h1>ToGathering</h1>
+        <p>A single page web application helps users find like-minded inviduals
+          by joining groups and events. Inspired by Meetup. Built with Rails,
+          React, Redux, Javascript, and PostgreSQL</p>
       </div>
-      <div>
+      <div className="pyromatics">
         <h1>Pyromatics</h1>
+        <p>Finalist at the SpaceApps Hackathon sponsored by NASA.
+          An application that crowdsources forest fires and shows safe zones on
+          the map. Built in MongoDB, React, Redux, Node.js, and Javascript</p>
       </div>
     </div>
   )
