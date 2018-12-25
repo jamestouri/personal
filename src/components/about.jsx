b274740caf4e492087268336bb8213f80718e525
jamestouri <j_touri@yahoo.com>
2018-12-24 19:27:31 -0800

    Add email

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 const AboutPage = props => {
   return (
     <div>
@@ -8,6 +8,8 @@ const AboutPage = props => {
         I love snowboarding and I can't get up to the Tahoe mountains enough during the season. Other hobbies include fitness and going to the movies
         I became a software engineer because I want to get involved in the space/robotics industry, and the best way to contribute to those companies is to help
         build the product they want to bring out onto the world. Other interests I have include building products that build jobs for users and individuals alike.</p>
+
+      <p>Interested in chatting? <a href = "mailto: jamestouri50@gmail.com">Send me a message</a></p>
     </div>
   )
 }
